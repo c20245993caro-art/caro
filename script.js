efeba735740b1c61bcacaf5d10c970bd49f904b8
c20245993caro-art <c20245993.caro@csav.edu.ph>
2026-09-04 14:37:35 +0800

    variavles (let and  const)

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 console.log("Hello World");
 console.log("From script.js file.");
+
+const myName = "CJ";
+let age = 12;
+let number = "09151011735";
+let address = "yawehhh";
+let sixseven = 67;
+
+console.log(`Name:  ${myName}` );
+console.log(`age:   ${age} `);
+console.log(`Number:  ${number}` );
+console.log(`address:  ${address}` );
+console.log(` ${sixseven}`);
+
+
+
+
