7b244ea7477d9bf1e7bb1f7bd9870a0d8487ab86
c20245993caro-art <c20245993.caro@csav.edu.ph>
2026-09-04 16:51:13 +0800

    textContent

--- script.js
+++ script.js
@@ -24,14 +24,18 @@ function greet (greetings,names=[5]){
 
 console.log(greet("Mayunggg aga" , ["cj","bai","soy","tol","erp"]));
 
-const heading = document.querySelector("h1");
-console.log(heading);
+const heading = document.querySelector("title");
+heading.textContent = "uling";
+console.log(heading.textContent);
+
 
 const contactHeading = document.querySelector("#contact h2")
 console.log(contactHeading)
 
 const servicesHeading = document.querySelector("#services h2")
 console.log(servicesHeading);
+
+
 
 
 
